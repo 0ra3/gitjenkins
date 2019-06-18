dcfd29ba982892829cdd973def041774f4e8a8bc
0ra3 <thelliez.alban@gmail.com>
2019-06-18 12:19:35 +0200

    first commit

--- FichierGit.java
+++ FichierGit.java
@@ -1,5 +1,5 @@
-public class Git{
-    public static main (String[] args){
+public class FichierGit{
+    public static void main (String[] args){
         System.out.println("A partir de GIT");
     }
 }
